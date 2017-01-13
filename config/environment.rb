d8298caf2671dce0a4ec9d075b981d8ef6a06def
Brie Coyle <brie.coyle@gmail.com>
2017-01-13 01:54:13 +0000

    Move require-relatives back tolib/tivo_helper.rb

--- config/environment.rb
+++
@@ -1,8 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-require_relative "tivo_helper/version"
-require_relative "tivo_helper/cli"
-require_relative "tivo_helper/show"
-require_relative "tivo_helper/scraper"
--- lib/tivo_helper.rb
+++ lib/tivo_helper.rb
@@ -1,4 +1,11 @@
 module TivoHelper
 end
 
-require_relative "../config/environment"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative "tivo_helper/version"
+require_relative "tivo_helper/cli"
+require_relative "tivo_helper/show"
+require_relative "tivo_helper/scraper"
